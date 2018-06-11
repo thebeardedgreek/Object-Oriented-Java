c34a1605258ead1f64780e8ff5e443492e2c0d3d
diwakar1 <diwakar1@umbc.edu>
2018-06-11 15:35:51 -0400

    hashmap Practice 6/11 3:35pm

---
+++ src/hashmapPractice.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+import static java.lang.System.out;
+import java.io.File;
+
+public class hashmapPractice {
+
+    static int userInteger;
+    static String userValue;
+    static HashMap<Integer, String> myMap = new HashMap<Integer, String>();
+    public static void main(String[] args) {
+
+        Scanner scannerInput = new Scanner(System.in);
+        userInteger = 0;
+        myMap.put(10,"ten");
+        myMap.put(11,"eleven");
+        myMap.put(12,"twelve");
+
+        Boolean keepLooping = true;
+        while (keepLooping) {
+            out.print("Enter a number in integer form:\n");
+
+            int userEntry = scannerInput.nextInt();
+            String value = myMap.get(userEntry);
+            if (value == null){
+                out.print("This number is not in the program; to add it, please type in the number as an integer:\n");
+                userInteger = scannerInput.nextInt();
+                out.print("Now, type the number as a word:\n");
+                userValue = scannerInput.next();
+            } else {
+                out.print(value + "\n");
+            }
+
+            myMap.put(userInteger, userValue);
+            out.print("Would you like to enter another number? (Y or N)\n");
+            String userChoice = scannerInput.next().toLowerCase();
+            if (userChoice.equals("n")) {
+                keepLooping = false;
+            }
+        }
+    }
+}
